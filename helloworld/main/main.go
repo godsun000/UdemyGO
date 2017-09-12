a3894d0e10abe15378115035e05d20236e6f930f
xuming <719423471@qq.com>
2017-09-12 23:57:41 +0800

    main

---
+++ helloworld/main/main.go
@@ -0,0 +1,14 @@
+package main
+import (
+	"fmt"
+	"github.com/UdemyTraining/helloworld/utility"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+	utility.Util()
+	//for i:=0;i<10;i++{
+	//	fmt.Printf("ten: %d \t binary: %b \t hex: %x \t utf-8: %q \n",i,i,i)
+	//
+	//}
+}
